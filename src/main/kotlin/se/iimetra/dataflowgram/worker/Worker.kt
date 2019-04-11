29e300d605b6c5d010f0b12d617eee0fdae341e0
Artemii Bezguzikov <artemii.bezguzikov@almworks.com>
2019-04-11 03:54:46 +0300

    add ping worker

--- src/main/kotlin/se/iimetra/dataflowgram/worker/Worker.kt
+++ src/main/kotlin/se/iimetra/dataflowgram/worker/Worker.kt
@@ -2,14 +2,37 @@ package se.iimetra.dataflowgram.worker
 
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import se.iimetra.dataflowgram.root.FunctionMeta
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.concurrent.CompletableFuture
+
 
 class Worker {
   private val actionQueue = Channel<WorkerAction>()
+  private lateinit var client: HelloWorldClient
+
+  init {
+    GlobalScope.launch {
+      try {
+        val process = ProcessBuilder("python", "python/server.py").start()
+        BufferedReader(InputStreamReader(process.inputStream)).use {
+          val tt = it.lines()
+          tt.forEach {
+            println(it)
+          }
+        }
+      } finally {
+        client.shutdown()
+      }
+    }
+  }
 
   fun start() = GlobalScope.launch {
+    delay(3000)
+    client = HelloWorldClient("localhost", 50051)
     while (true) {
       val action = actionQueue.poll()
       when (action) {
@@ -18,7 +41,8 @@ class Worker {
         }
         is WorkerAction.Execute -> {
           println("Execute $action")
-          action.resultRef.complete("123!")
+          val response = client.greet(action.function.toString())
+          action.resultRef.complete("answer: $response")
         }
       }
     }
